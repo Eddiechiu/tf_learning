b5d52cbfdd5b35995637f1d809cb92e698154a2c
Eddiechiu <eddiechiu77@sina.com>
2016-11-23 05:28:32 -0800

    add abstract match

--- paperTitleGrab.py
+++ paperTitleGrab.py
@@ -1,28 +1,39 @@
 from bs4 import BeautifulSoup
 import re
-import urllib.request
+import urllib2
 
-def title_grab(page_num,pattern):
+def content_grab(pageNum, pattern):
     url = 'http://ieeexplore.ieee.org/xpl/mostRecentIssue.jsp?...' \
-          'punumber=77&filter%3DAND%28p_IS_Number%3A7368234%29&pageNumber=' + str(page_num)
-    headers = ('User-Agent', 'Mozilla/5.0...'
-                         ' (Windows NT 10.0; Win64; x64)...'
-                         ' AppleWebKit/537.36 (KHTML, like Gecko)...'
-                         ' Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586')
-    urlopener = urllib.request.build_opener()
-    urlopener.addheaders = [headers]
-    data = urlopener.open(url).read()
+          'punumber=77&filter%3DAND%28p_IS_Number%3A7368234%29&pageNumber=' + str(pageNum)
+    headers = {'User-Agent': 'Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'}
+    req = urllib2.Request(url,headers=headers)
+    response = urllib2.urlopen(req)
+    data = response.read()
     data = data.decode('GBK')
     return pattern.findall(data)
 
-pattern = re.compile(r'<span id="art-abs-title-\d{7}">(.*?)</span>')
+pattern_title = re.compile(r'<span id="art-abs-title-\d{7}">(.*?)</span>')
+pattern_abstract = re.compile(r'<p>(\W)*(.*?)<a href="/document')
 paper_titles = []
-
-for num in range(1, 2):
+paper_abstracts = []
+for num in range(1,2):
     print('page ' + str(num) + ' start, wait...')
-    for title in title_grab(page_num=num, pattern=pattern):
+
+    for title in content_grab(pageNum=num, pattern=pattern_title):
       paper_titles.append(title)
+
+    for abstract in content_grab(pageNum=num, pattern=pattern_abstract):
+      paper_abstracts.append(abstract)
+
     print('page ' + str(num) + ' finished.')
 
+i = 1
 for title in paper_titles:
-    print(title)
+    print str(i) + '. ' + title
+    i = i + 1
+
+i = 1
+print(paper_abstracts)
+for abstract in paper_abstracts:
+    print str(i) + '. ' + abstract
+    i = i + 1
