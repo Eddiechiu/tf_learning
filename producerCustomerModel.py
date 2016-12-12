e870f2be85b2a111e7f7e05e124832655e66bbb8
Eddiechiu <eddiechiu77@sina.com>
2016-12-12 04:54:35 -0800

    add another queue to producer and customer model

--- producerCustomerModel.py
+++ producerCustomerModel.py
@@ -2,23 +2,36 @@ import threading, time
 import Queue
 import random
 
-q = Queue.Queue()
+q1 = Queue.Queue()
+q2 = Queue.Queue()
 
-def Producer(name):
-	for i in range(20):
-		q.put(i)
-		print '+++++ Producer has made Item %s +++' % i
-		time.sleep(random.randrange(3))
+def ProducerA():
+	for i in range(10):
+		q1.put(i)
+		print '*** A has made Item %s ***' % i
+		time.sleep(random.randrange(2))
 
-def Consumer(name):
+def ProducerB():
 	count = 0
-	while count < 20:
-		print '+++ Consumer has got Item %s +++' % q.get()
+	while count < 10:
+		i = q1.get()
+		print '+++ B get Item %s from A and complete Item %s +++' % (i, i)
+		q2.put(i)
 		count += 1
 		time.sleep(random.randrange(2))
 
-thread_p = threading.Thread(target=Producer, args=('Derong', ))
-thread_c = threading.Thread(target=Consumer, args=('Eddie', ))
 
-thread_p.start()
+def Consumer():
+	count = 0
+	while count < 10:
+		print '--- Consumer has got Item %s ---' % q2.get()
+		count += 1
+		time.sleep(random.randrange(3))
+
+thread_p1 = threading.Thread(target=ProducerA)
+thread_p2 = threading.Thread(target=ProducerB)
+thread_c = threading.Thread(target=Consumer)
+
+thread_p1.start()
+thread_p2.start()
 thread_c.start()
