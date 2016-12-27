f3b11fa10e4dcca276a71c68380685737c728018
Deron Chiu <eddiechiu77@sina.com>
2016-12-27 18:26:28 +0800

    testify the multiprocessing with return value

--- multi_process.py
+++ multi_process.py
@@ -3,17 +3,16 @@ import os, time
 
 def some_task(name):
 	print('Task %s (%s) is running...' % (name, os.getpid()))
-	start = time.time()
-	time.sleep(2)
-	end = time.time()
-	print('Task %s runs for %0.2f seconds and it is now %s' % (name, (end-start), end))
+	return name
 
-if __name__=='__main__':	
+
+if __name__=='__main__':
+	a = []
 	print('Parent process %s' % os.getpid())
 	p = Pool()
 	for i in range(9):
-		p.apply_async(some_task, args=(i,))
+		a.append(p.apply_async(some_task, args=(i, )))
 	print('waiting for all subprocesses')
 	p.close()
 	p.join()
-	print('done')
+	print([res.get() for res in a])
