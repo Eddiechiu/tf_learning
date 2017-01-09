f49a4e7ad9188ad85bbd4ff8a0d5144f37499ff7
eddie <eddiechiu77@sina.com>
2017-01-09 23:37:00 +0800

    little change

--- myFirstScrapy/myFirstScrapy/spiders/dmoz_spider.py
+++ myFirstScrapy/myFirstScrapy/spiders/dmoz_spider.py
@@ -9,6 +9,12 @@ class DomzSpider(scrapy.spiders.Spider):
 	]
 
 	def parse(self, response):
-		filename = response.url.split('/')[-2]
-		with open(filename, 'wb') as f:
-			f.write(response.body)
+		# response.url.split('/')[-2] 相当于把上面的url地址用/隔开，然后把Books、Resources取出来。[-1]是''(啥都没有)
+		# filename = response.url.split('/')[-2]
+		# with open(filename, 'wb') as f:
+		# 	f.write(response.body)
+		for sel in response.xpath('//ul/li'):
+			title = sel.xpath('a/text()').extract()
+			link = sel.xpath('a/@href').extract()
+			desc = sel.xpath('text()').extract()
+			print(title, link, desc)
