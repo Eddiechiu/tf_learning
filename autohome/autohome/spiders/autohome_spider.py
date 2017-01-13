bf031d4b83c1dca6a128d59e91a7510f952dc4c1
Derong Qiu <eddiechiu77@sina.com>
2017-01-13 11:25:57 +0800

    get the regular expression for paper amount

--- autohome/autohome/spiders/autohome_spider.py
+++ autohome/autohome/spiders/autohome_spider.py
@@ -23,3 +23,8 @@ class AutohomeSpider(scrapy.spiders.Spider):
 			item['car_name'] = sel.xpath('@title').extract()
 			item['car_club_url'] = sel.xpath('@href').extract()
 			yield item
+
+		# regular expression for the number of papers in the club:
+		# re.findall(r'<span>(.*?)</span>', response.xpath('//div[@class="pagearea"]/div[@class="fl"]/span').extract())
+		# response is from url='http://club.autohome.com.cn/bbs/forum-c-692-1.html'
+		# (NOTICE here [@class="xxxx"], "" must be used, which cannot be replaced by '')
